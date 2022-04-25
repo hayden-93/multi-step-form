94320a05e202c3fc4d7aec91ccde69bfbe75bcd4
Hayden Buckley-Smith <hayden@abstack.co>
2022-04-25 10:47:23 +0100

    build: App Setup
    Created React App with Typescript to make a multi-step form
    Installed Tailwind CSS framework to style the App
    Installed .prettierrc for formatting
    Stripped code to start app fresh from unused logos, styles and files

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <p className='bg-black text-cyan-500'>Hi, Hayden</p>
+    </div>
+  );
+}
